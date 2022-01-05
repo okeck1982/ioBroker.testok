9cdda61e233485855bfe3693ab0e83c790e16cd1
okeck1982 <97165003+okeck1982@users.noreply.github.com>
2022-01-05 21:02:28 +0100

    Initial Commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"test_adapter_ok adapter settings": {
+		"en": "Adapter settings for test_adapter_ok",
+		"de": "Adaptereinstellungen für test_adapter_ok",
+		"ru": "Настройки адаптера для test_adapter_ok",
+		"pt": "Configurações do adaptador para test_adapter_ok",
+		"nl": "Adapterinstellingen voor test_adapter_ok",
+		"fr": "Paramètres d'adaptateur pour test_adapter_ok",
+		"it": "Impostazioni dell'adattatore per test_adapter_ok",
+		"es": "Ajustes del adaptador para test_adapter_ok",
+		"pl": "Ustawienia adaptera dla test_adapter_ok",
+		"zh-cn": "test_adapter_ok的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
